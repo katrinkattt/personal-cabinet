3406a4898fa0049d416e3706ec57e5535ae6466e
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-01 22:51:57 +0500

    Create constants.js

---
+++ static/js/cabinet/constants.js
@@ -0,0 +1,8 @@
+const tableBody = $('#tableBody');
+const addOrder = $('#add-order');
+const btnOpenModal = $('#openModal');
+
+const orderInput = $('#order-input');
+const ownerInput = $('#owner-input');
+const priceInput = $('#price-input');
+const commentInput = $('#comment-input');
